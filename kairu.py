d6c04e990842550dc0c51f70a280a1842d496441
Elijah MacPherson <elijah.macpherson@gmail.com>
2020-11-21 14:43:38 -0500

    Initial project creation
    Created python 3.7 project with basic 'hello' command.

---
+++ kairu.py
@@ -0,0 +1,37 @@
+import os
+
+import discord
+from dotenv import load_dotenv
+import logging
+
+load_dotenv()
+logger = logging.getLogger('discord')
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    guild = discord.utils.get(client.guilds, name=GUILD)
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+@client.event
+async def on_message(message):
+    if (message.author == client.user):
+        return
+    
+    if (message.content.startswith('$hello')):
+        await message.channel.send('Hello!')
+    
+
+client.run(TOKEN)
